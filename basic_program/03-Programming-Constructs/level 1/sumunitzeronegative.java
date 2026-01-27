b7f3bd54984e6960c62d2e1c9c6bb06c004843c8
Krish rai <krishrai311@gmail.com>
2026-01-27 21:13:28 +0530

    [krish rai]forloop

---
+++ basic_program/03-Programming-Constructs/level 1/sumunitzeronegative.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class SumUntilZeroOrNegative{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		double total = sc.nextDouble();
+		 
+		while(true){
+			double num = sc.nextDouble();
+			if(num<=0){
+				break;
+			}
+			total = total+num;
+		}
+		System.out.println("Total sum = "+total);
+	}
+}
